262549601de13904841ff3033068f5062c572647
Charles <krowven@gmail.com>
2022-09-18 21:22:04 -0500

    Updated configs, added test data

--- public/js/config.js
+++ public/js/config.js
@@ -1,20 +1,48 @@
 // Parses a given config file
 
 class Config {
-    constructor(file_name) {
-        this.config_file = file_name
-        this.config = {}
+    constructor() {
+
     }
-    open(file_name) {
-        if (!file_name) file_name = this.config_file
-        if (!file_name.includes("json")) {
+    
+    async initialize() {
+        await this.readConfig()
+    }
+
+    static async create(file_name) {
+        // Config wrapper to initialize with the new config file
+        const conf = new Config();
+        conf.config_file = file_name
+        await conf.initialize(file_name)
+        return conf
+    }
+
+    get DOMAIN() {
+        return this.config?.DOMAIN ? this.config.DOMAIN : "https://cwms-data.usace.army.mil/cwms-data"
+    }
+    get OFFICE() {
+        return this.config?.OFFICE ? this.config.OFFICE : ""
+    }
+    get lakes() {
+        return this.config?.projects?.lakes
+    }
+    get projects() {
+        // projects = lakes in this context
+        return this.lakes()
+    }
+
+    async readConfig() {
+        this.config = await this.openConfig()
+    }
+    async openConfig() {
+        if (!this.config_file) throw Error("You must specify a config file on the create('config file.json') method")
+        if (!this.config_file.includes("json")) {
             alert("Invalid File, must be a JSON file. (.json)")
             return {}
         }
-        return fetch(this.config_file)
-            .then(res.json())
+        return await fetch(this.config_file)
+            .then(res => res.json())
             .then(data=> {
-                this.config = data
                 return data
             })
             .catch((err)=> {
@@ -25,3 +53,5 @@ class Config {
 
     }
 }
+
+export default Config
